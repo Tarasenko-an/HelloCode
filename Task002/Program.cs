de8a6302b321500989084f8fcd99ac43586e3aa9
Anton_Tarasenko <tarasenko-an@ngs.ru>
2022-08-31 12:02:46 +0700

    Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,17 @@
+﻿//Вводим первое число
+Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+//Вводим второе число
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+//Проверяем на условие и выводим результат
+if (num1*num1 == num2)
+   {
+        Console.WriteLine("Да");
+   } 
+else
+    {
+        Console.WriteLine("Нет");
+    }    
