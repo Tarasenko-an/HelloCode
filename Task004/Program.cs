1f17bf3af192a617192a45d60837f3ff07dbfee2
Anton_Tarasenko <tarasenko-an@ngs.ru>
2022-08-31 12:01:45 +0700

    Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,10 @@
+﻿//Введите число
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = -Math.Abs(num);
+
+while (count <= Math.Abs(num))
+{
+    Console.Write($"{count} ");
+    count++;
+}
