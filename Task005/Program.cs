e1da398c8abaa1556978b82ca93b15e65942f401
Anton_Tarasenko <tarasenko-an@ngs.ru>
2022-08-31 12:03:02 +0700

    Task005

---
+++ Task005/Program.cs
@@ -0,0 +1,14 @@
+﻿//Введим число
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+//Проверка на 3х-значное число
+if (((num > -999) && (num < -100)) || (num > 100) && (num < 1000))
+
+{
+   Console.WriteLine(Math.Abs(num%10)); 
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
